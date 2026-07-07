55fe4ba9d09e7a3e7fa80f43a62c6fb8d1320b57
pranaydas-BW <pranay.das@broadwaylive.in>
2026-07-07 11:26:59 +0530

    fix: bump bundle to v45 to bust CDN cache

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v44.js`,
-        chunkFileNames: `assets/[name]-[hash]-v44.js`,
+        entryFileNames: `assets/[name]-[hash]-v45.js`,
+        chunkFileNames: `assets/[name]-[hash]-v45.js`,
       }
     }
   }
