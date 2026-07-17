b870c2016b19f40357726cd4485df687529dcad3
pranaydas-BW <pranay.das@broadwaylive.in>
2026-07-17 15:04:17 +0530

    fix: bump bundle to v76 to bust CDN cache

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v75.js`,
-        chunkFileNames: `assets/[name]-[hash]-v75.js`,
+        entryFileNames: `assets/[name]-[hash]-v76.js`,
+        chunkFileNames: `assets/[name]-[hash]-v76.js`,
       }
     }
   }
